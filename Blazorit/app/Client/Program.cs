e63ee54b35d075359e4a07ce884f5e258e1721a3
semukov <avsemukov@yahoo.com>
2022-11-28 16:50:08 +0300

    Fast-FluentUI added in domain layout

--- Blazorit/app/Client/Program.cs
+++ Blazorit/app/Client/Program.cs
@@ -6,6 +6,7 @@ using Blazored.LocalStorage;
 using Microsoft.AspNetCore.Components.Authorization;
 using Microsoft.AspNetCore.Components.Web;
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
+using Microsoft.Fast.Components.FluentUI;
 
 var builder = WebAssemblyHostBuilder.CreateDefault(args);
 builder.RootComponents.Add<App>("#app");
@@ -13,6 +14,7 @@ builder.RootComponents.Add<HeadOutlet>("head::after");
 
 builder.Services.AddBlazoredLocalStorage(); //custom add
 builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
+builder.Services.AddFluentUIComponents(); //Fast-FluentUI
 builder.Services.AddAntDesign(); //AntDesign
 builder.Services.AddScoped<IIdentityService, IdentityService>(); //custom add
 builder.Services.AddOptions(); //custom add
