d395bad045471a46992db21dc2ff156d4dadf8ed
semukov <avsemukov@yahoo.com>
2022-12-10 17:03:37 +0300

    Naming CSS fixed

--- Blazorit/app/Client/Program.cs
+++ Blazorit/app/Client/Program.cs
@@ -6,14 +6,12 @@ using Blazored.LocalStorage;
 using Microsoft.AspNetCore.Components.Authorization;
 using Microsoft.AspNetCore.Components.Web;
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
-using MudBlazor.Services;
 
 var builder = WebAssemblyHostBuilder.CreateDefault(args);
 builder.RootComponents.Add<App>("#app");
 builder.RootComponents.Add<HeadOutlet>("head::after");
 builder.Services.AddBlazoredLocalStorage(); //custom add
 builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
-builder.Services.AddMudServices(); //MudBlazor
 builder.Services.AddAntDesign(); //AntDesign
 builder.Services.AddScoped<IIdentityService, IdentityService>(); //custom add
 builder.Services.AddOptions(); //custom add
