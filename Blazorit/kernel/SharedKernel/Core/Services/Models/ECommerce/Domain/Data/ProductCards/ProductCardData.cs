c17c423ecedb1e66e9629710895b3f07e43d6074
semukov <avsemukov@yahoo.com>
2023-02-16 12:03:29 +0300

    Writing code

--- Blazorit/kernel/SharedKernel/Core/Services/Models/ECommerce/Domain/Data/ProductCards/ProductCardData.cs
+++ Blazorit/kernel/SharedKernel/Core/Services/Models/ECommerce/Domain/Data/ProductCards/ProductCardData.cs
@@ -11,22 +11,33 @@ namespace Blazorit.SharedKernel.Core.Services.Models.ECommerce.Domain.Data.Produ
     {
         public long Id { get; set; }
 
-        public string Name { get; set; } = null!;
+        public string Name { get; set; } = string.Empty;
 
-        public string Sku { get; set; } = null!;
+        public string Sku { get; set; } = string.Empty;
 
         public decimal Price { get; set; }
 
-        public string Description { get; set; } = null!;
+        public string Description { get; set; } = string.Empty;
 
-        public string LinkPart { get; set; } = null!;
+        public string LinkPart { get; set; } = string.Empty;
 
-        public string Category { get; set; } = null!;
+        public string Category { get; set; } = string.Empty;
 
-        public string CategoryFullName { get; set; } = null!;
+        public string CategoryFullName { get; set; } = string.Empty;
 
         public IEnumerable<PictureLinkPart> PicturesLinkParts { get; set; } = Enumerable.Empty<PictureLinkPart>();
 
-        public string MainPictureLinkPart { get; set; } = null!;
+        public string MainPictureLinkPart { get; set; } = string.Empty;
+
+        /// <summary>
+        /// Price for user view
+        /// </summary>
+        public string StrPrice
+        {
+            get
+            {
+                return Price.ToString("N0");
+            }
+        }
     }
 }
